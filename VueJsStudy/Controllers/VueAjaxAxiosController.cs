8c954a51e4c2c8b4c3fb5714e34251ec56b15802
LC <1784455078@qq.com>
2021-05-10 11:28:02 +0800

    添加表格显示

---
+++ VueJsStudy/Controllers/VueAjaxAxiosController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VueJsStudy.Controllers
+{
+    public class VueAjaxAxiosController : Controller
+    {
+        // GET: VueAjaxAxios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList()
+        {
+
+            List<Student> list = new List<Student> {
+                new Student { Name = "LC",Sex="男" ,StudentNumber="124545"},
+                new Student { Name = "陈铭",Sex="女",StudentNumber="20153356"}
+            };
+
+
+            return Json(new { List = list, IsTrue = true },JsonRequestBehavior.AllowGet);
+
+        }
+
+
+
+
+
+
+
+
+
+
+        public ActionResult TestWeb() {
+
+
+            return View();
+
+        }
+
+        public class Student
+        {
+
+            public string Name { get; set; }
+            public string Sex { get; set; }
+            public string StudentNumber { get; set; }
+
+
+
+
+        }
+
+
+
+    }
+}
